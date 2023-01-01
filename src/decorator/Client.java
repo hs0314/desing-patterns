690cea6256036f9826e703fff81ab31cced0896a
heesu <tedlee0314@gmail.com>
2023-01-01 15:49:10 +0900

    구조 패턴 정리2 - 데코레이터 패턴

---
+++ src/decorator/Client.java
@@ -0,0 +1,13 @@
+package decorator;
+
+public class Client {
+    private CommentService commentService;
+
+    public Client(CommentService commentService){
+        this.commentService = commentService;
+    }
+
+    public void writeComment(String comment){
+        commentService.addComment(comment);
+    }
+}
